6c8a4b8e971a13c2450a0a3f3b126c1acdcc84b9
Aditya Dhade <aditya.dhade@rtcamp.com>
2025-01-07 00:45:30 +0530

    --wip-- [skip ci]

--- plugins/optimization-detective/admin/prime-url-metrics.php
+++ plugins/optimization-detective/admin/prime-url-metrics.php
@@ -10,10 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( isset( $_GET['od_prime'] ) ) {
-	add_filter( 'od_url_metric_storage_lock_ttl', '__return_zero' );
-}
-
 /**
  * Class containing logic for locking storage for new URL Metrics.
  *
@@ -72,7 +68,9 @@ function od_enqueue_admin_assets( string $hook_suffix ): void {
 	sort( $bp_widths );
 	$bp_widths[] = end( $bp_widths ) + 100;
 
+	// We need to ensure min is 1 else the height will become too small.
 	$min_ar = max( 1, od_get_minimum_viewport_aspect_ratio() );
+	// We also need to ensure max is 2 else the height will become too large.
 	$max_ar = min( 2, od_get_maximum_viewport_aspect_ratio() );
 
 	// Get minimum and maximum widths for interpolation.
@@ -87,7 +85,7 @@ function od_enqueue_admin_assets( string $hook_suffix ): void {
 				$ar = $min_ar;
 			} else {
 				// Calculate aspect ratio.
-				$ar = $max_ar - ( ($max_ar - $min_ar) * ( ( $width - $min_width ) / ( $max_width - $min_width ) ) );
+				$ar = $max_ar - ( ( $max_ar - $min_ar ) * ( ( $width - $min_width ) / ( $max_width - $min_width ) ) );
 			}
 
 			// Ensure aspect ratio does not go below min_ar.
@@ -125,18 +123,27 @@ add_action( 'admin_enqueue_scripts', 'od_enqueue_admin_assets' );
  * @return string[] List of URLs.
  */
 function od_generate_urls_list(): array {
-	$posts = get_posts(
-		array(
-			'post_type'      => 'any',
-			'post_status'    => 'publish',
-			'posts_per_page' => 20,
-		)
-	);
+	$server   = wp_sitemaps_get_server();
+	$registry = $server->registry;
 
-	$urls = array();
-	foreach ( $posts as $post ) {
-		$urls[] = get_permalink( $post );
+	$all_urls = array();
+
+	// Loop over all registered providers (e.g., posts, users, taxonomies).
+	$providers = $registry->get_providers();
+	foreach ( $providers as $provider_key => $provider ) {
+		// Each provider may have one or more object subtypes (e.g., 'post', 'page', 'attachment').
+		$subtypes = $provider->get_object_subtypes();
+		foreach ( $subtypes as $key => $subtype ) {
+			$max_num_pages = $provider->get_max_num_pages( $key );
+
+			// The provider may have multiple pages of URLs as one page may not be enough to get all URLs.
+			for ( $page = 1; $page <= $max_num_pages; $page++ ) {
+				$url_list = $provider->get_url_list( $page, $key );
+				if ( ! empty( $url_list ) && is_array( $url_list ) ) {
+					$all_urls = array_merge( $all_urls, array_filter( array_column( $url_list, 'loc' ) ) );
+				}
+			}
+		}
 	}
-
-	return array_values( array_filter( $urls ) );
+	return $all_urls;
 }
